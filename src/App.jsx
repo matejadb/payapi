daefcc279ec7b93257cb1d2ac0b4330289d00060
matejadb <92729211+matejadb@users.noreply.github.com>
2026-03-09 13:16:33 +0100

    set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+
+import AppLayout from './ui/AppLayout';
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 function App() {
 	return (
-		<>
-			<div>APP</div>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<AppLayout />}>
+					<Route index element={<Navigate replace to="home" />} />
+					<Route path="home" element={<Home />} />
+					<Route path="pricing" element={<Pricing />} />
+					<Route path="about-us" element={<About />} />
+					<Route path="contact-us" element={<Contact />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
